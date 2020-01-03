ff451f8a17327ed873ed4d85d7eccf6696547aa9
unknown <jpainter@calibrateconsulting.com>
2020-01-03 17:00:50 +0000

    Create basic app structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Speed Typing Game</h1>
+      <textarea />
+      <h4>Time Remaining</h4>
+      <button>Start Game</button>
+      <h1>Word Count</h1>
     </div>
   );
 }
