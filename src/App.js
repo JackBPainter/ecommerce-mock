626588fe7933eca983d8b7b7422ecb42f4820244
unknown <jpainter@calibrateconsulting.com>
2020-01-03 17:53:30 +0000

    Create calculateWordCount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const calculateWordCount = (text) => { // The text in the argument will not mutate the text in state as it's been passed down
+    console.log(text === "" ? 0 : text.trim().split(" ").length)
+    return text.trim().split(" ").length
+  }
+
   return (
     <div className="App">
       <h1>Speed Typing Game</h1>
-      <textarea />
+      <textarea onChange={handleChange} />
       <h4>Time Remaining</h4>
-      <button>Start Game</button>
-      <h1>Word Count</h1>
+      <button onClick={() => calculateWordCount(text)}>Start Game</button>
+      <h1>Word Count: </h1>
     </div>
   );
 }
