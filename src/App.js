c6ae663527b539fed313679f4589fb19471ae2cb
unknown <jpainter@calibrateconsulting.com>
2020-01-06 19:26:52 +0000

    Add disable functionality to text area & button and create endGame button

--- src/App.js
+++ src/App.js
@@ -2,39 +2,60 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(10);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const start_time = 5
 
-  console.log(isTimeRunning)
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(start_time);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0)
+
 
   const handleChange = (e) => {
     setText(e.target.value)
   }
 
   const calculateWordCount = (text) => { // The text in the argument will not mutate the text in state as it's been passed down
-    console.log(text.trim().split(" ").filter(word => word !== "").length)
     return text.trim().split(" ").filter(word => word !== "").length
   }
 
+  const startGame = () => {
+    setIsTimeRunning(true)
+    setTimeRemaining(start_time)
+    setText("")
+  }
+
+  const endGame = () => {
+    setIsTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+  }
+
   useEffect(() => {
-      if (isTimeRunning && timeRemaining > 0) {
-        setTimeout(() => {
-          setTimeRemaining(time => time - 1)
-        }, 1000)
-      } else {
-        setIsTimeRunning(false)
-      }
-    
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    } else {
+      endGame()
+    }
+
   }, [timeRemaining, isTimeRunning])
 
   return (
     <div className="App">
       <h1>Speed Typing Game</h1>
-      <textarea onChange={handleChange} />
+      <textarea
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+      />
       <h4>Time Remaining: {timeRemaining}</h4>
-<button onClick={() => setIsTimeRunning(true) && calculateWordCount(text)}>Start Game</button>
-      <h1>Word Count: </h1>
+      <button
+        disabled={isTimeRunning}
+        onClick={startGame}
+      >
+        Start Game
+      </button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
