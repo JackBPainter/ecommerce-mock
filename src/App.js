f3ee5dc7f92604e23b51de31419552d0409f97fd
unknown <jpainter@calibrateconsulting.com>
2020-01-06 08:55:41 +0000

    Create time remaining function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(10);
 
   const handleChange = (e) => {
     setText(e.target.value)
@@ -13,11 +14,19 @@ function App() {
     return text.trim().split(" ").filter(word => word !== "").length
   }
 
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    }
+  }, [timeRemaining])
+
   return (
     <div className="App">
       <h1>Speed Typing Game</h1>
       <textarea onChange={handleChange} />
-      <h4>Time Remaining</h4>
+      <h4>Time Remaining: {timeRemaining}</h4>
       <button onClick={() => calculateWordCount(text)}>Start Game</button>
       <h1>Word Count: </h1>
     </div>
