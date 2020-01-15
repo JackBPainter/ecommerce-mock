79a3d29f50619e2358d459b09ac765e9be77cb59
unknown <jackbpainter@gmail.com>
2020-01-15 08:39:51 +0000

    Create the applications basic structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
-function App() {
-
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+function App() {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import 'remixicon/fonts/remixicon.css'
+
+function Header() {
+    return (
+        <header>
+            <h2>Pic Some</h2>
+            <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        </header>
+    )
+}
+
+export default Header
