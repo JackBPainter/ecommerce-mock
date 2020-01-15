fe04f791f9450e6dff323e3577469bb00c009901
unknown <jackbpainter@gmail.com>
2020-01-15 09:09:04 +0000

    Setup app file structure and create basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
+
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 
-function App() {    
+
+function App() {
     return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-        </div>
+            <div>
+                <Header />
+                <h1>Home Page</h1>
+                <Switch>
+                    <Route exact path="/">
+                        <Photos />
+                    </Route>
+                    <Route path="/cart">
+                        <Cart />
+                    </Route>
+                </Switch>
+            </div>
     )
 }
 
