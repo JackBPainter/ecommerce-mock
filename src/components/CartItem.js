c55bedf2615b4cc38d117c79784b637774bfe38b
unknown <jackbpainter@gmail.com>
2020-01-20 18:24:32 +0000

    Create CartItem.js

---
+++ src/components/CartItem.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const CarItem = ({item}) => {
+    return (
+        <div className="cart-item">
+            <i className="ri-delete-bin-line"></i>
+            <img src={item.url} width="130px" alt=""/>
+            <p>$5.99</p>
+        </div>
+    )
+}
+
+export default CarItem
