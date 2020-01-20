294ddb5f22365f92a0f64be24e7b0dd8fdd9643e
unknown <jackbpainter@gmail.com>
2020-01-20 11:23:32 +0000

    Update the headerCartIcon to show it filled if anything has been added to the cartItems

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,26 @@
 import React from "react"
 import 'remixicon/fonts/remixicon.css'
 import { Link } from "react-router-dom";
+import { AppContext } from "../Context"
 
 const Header = () => {
+    const { cartItems } = React.useContext(AppContext)
+
+    const headerCartIcon = cartItems.length > 0 ? 
+        "ri-shopping-cart-fill" :
+        "ri-shopping-cart-line"
+
     return (
         <header>
             <Link to="/">
                 <h2>Pic Some</h2>
             </Link>
             <Link to="/cart">
-                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+                <i className={`${headerCartIcon} ri-fw ri-2x`}></i>
             </Link>
         </header>
     )
 }
 
 export default Header
+
