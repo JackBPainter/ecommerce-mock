181e880e320b65d55fddf0d3e184b98e223e1d18
unknown <jackbpainter@gmail.com>
2020-01-20 19:03:11 +0000

    Create totalCost function in Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,18 +1,23 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import {AppContext} from "../Context";
 import CartItem from "../components/CartItem"
 
 const Cart = () => {
     const {cartItems} = useContext(AppContext)
     const cartItemElements = cartItems.map(item => (
-        <CartItem key={item.id} item={item} />
+        <CartItem key={item.id} item={item}/>
     ))
+ 
+    const totalCost = () => {
+        const total = cartItems.length * 5.99
+        return total.toLocaleString("en-UK", {style: "currency", currency: "GBP"})
+    }
 
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalCost()}</p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
