e9837228c2fc7512bf641d3b717cb1665be54b02
unknown <jackbpainter@gmail.com>
2020-01-21 17:52:53 +0000

    Add propTypes for item prop in CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,24 +1,32 @@
 import React, { useContext, useState } from "react"
 import { AppContext } from "../Context"
+import PropTypes from "prop-types"
 
-const CartItem = ({item}) => {
+const CartItem = ({ item }) => {
     const [hovered, setHovered] = useState(false)
-    const {removeFromCart} = useContext(AppContext)
+    const { removeFromCart } = useContext(AppContext)
 
     const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+    console.log(item)
 
     return (
         <div className="cart-item">
-                <i 
-                    className={iconClassName}
-                    onMouseEnter={() => setHovered(true)} 
-                    onMouseLeave={() => setHovered(false)} 
-                    onClick={() => removeFromCart(item.id)}>
-                </i>
-            <img src={item.url} width="130px" alt=""/>
+            <i
+                className={iconClassName}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                onClick={() => removeFromCart(item.id)}>
+            </i>
+            <img src={item.url} width="130px" alt="" />
             <p>£5.99</p>
         </div>
     )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        url: PropTypes.string.isRequired
+    })
 }
 
 export default CartItem
