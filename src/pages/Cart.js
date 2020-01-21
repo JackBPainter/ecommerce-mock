7648fe75eb57f2bd6155e2e3a2869f626134b5ae
unknown <jackbpainter@gmail.com>
2020-01-21 09:33:53 +0000

    Create placeOrderButton

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,8 @@ import {AppContext} from "../Context";
 import CartItem from "../components/CartItem"
 
 const Cart = () => {
-    const {cartItems} = useContext(AppContext)
+    const [buttonText, setButtonText] = useState("Place Order")
+    const {cartItems, setCartItems} = useContext(AppContext)
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item}/>
     ))
@@ -13,13 +14,22 @@ const Cart = () => {
         return total.toLocaleString("en-UK", {style: "currency", currency: "GBP"})
     }
 
+    const placeOrderButton = () => {
+        setButtonText("Ordering...")
+        setTimeout(() => {
+            console.log("Order Placed!")
+            setButtonText("Place Order")
+            setCartItems([])
+        }, 3000) 
+    }
+
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
             <p className="total-cost">Total: {totalCost()}</p>
             <div className="order-button">
-                <button>Place Order</button>
+            <button onClick={placeOrderButton}>{buttonText}</button>
             </div>
         </main>
     )
